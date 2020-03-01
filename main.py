29d307326e925846fb9ee93ca3bf39511b6aa679
danijoke <40579556+fd4n9@users.noreply.github.com>
2020-03-01 16:45:13 +0300

    thats all

--- main.py
+++ main.py
@@ -12,24 +12,20 @@ x = random.choice(computer)
 while user != 'Rock' or user != 'Paper' or user != 'Scissors':
     user = str(input("Rock, Scissors or Paper? \n"))
     if user == 'Rock' or user == 'Paper' or user == 'Scissors':
+        if user == computer:
+            print('Tie!\n')
         if user == 'Rock':
             if x == 'Paper':
-                print(x, '\nYou lose!')
+                print(x, '\nYou lose!\n')
             elif x == 'Scissors':
-                print(x, '\nYou win!')
-            else:
-                print(x, '\nStandoff')
+                print(x, '\nYou win!\n')
         if user == 'Paper':
             if x == 'Rock':
-                print(x, '\nYou win!')
+                print(x, '\nYou win!\n')
             elif x == 'Scissors':
-                print(x, '\nYou lose!')
-            else:
-                print(x, '\nStandoff')
+                print(x, '\nYou lose!\n')
         if user == 'Scissors':
             if x == 'Paper':
-                print(x, '\nYou win!')
+                print(x, '\nYou win!\n')
             elif x == 'Rock':
-                print(x, '\nYou lose!')
-            else:
-                print(x, '\nStandoff')
+                print(x, '\nYou lose!\n')
