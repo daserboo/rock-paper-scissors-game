9056c4e37dfd6f8af01ca241990017405e35362e
danijoke <40579556+fd4n9@users.noreply.github.com>
2020-03-01 14:45:17 +0300

    game is ready

--- main.py
+++ main.py
@@ -9,26 +9,27 @@ computer = ['Rock', 'Scissors', 'Paper']
 
 x = random.choice(computer)
 
-if user == 'Rock':
-    if x == 'Paper':
-        print(x, '\nYou lose!')
-    elif x == 'Scissors':
-        print(x, '\nYou win!')
-    else:
-        print(x, '\nStandoff')
-
-if user == 'Paper':
-    if x == 'Rock':
-        print(x, '\nYou win!')
-    elif x == 'Scissors':
-        print(x, '\nYou lose!')
-    else:
-        print(x, '\nStandoff')
-
-if user == 'Scissors':
-    if x == 'Paper':
-        print(x, '\nYou win!')
-    elif x == 'Rock':
-        print(x, '\nYou lose!')
-    else:
-        print(x, '\nStandoff')
+while user != 'Rock' or user != 'Paper' or user != 'Scissors':
+    user = str(input("Rock, Scissors or Paper? \n"))
+    if user == 'Rock' or user == 'Paper' or user == 'Scissors':
+        if user == 'Rock':
+            if x == 'Paper':
+                print(x, '\nYou lose!')
+            elif x == 'Scissors':
+                print(x, '\nYou win!')
+            else:
+                print(x, '\nStandoff')
+        if user == 'Paper':
+            if x == 'Rock':
+                print(x, '\nYou win!')
+            elif x == 'Scissors':
+                print(x, '\nYou lose!')
+            else:
+                print(x, '\nStandoff')
+        if user == 'Scissors':
+            if x == 'Paper':
+                print(x, '\nYou win!')
+            elif x == 'Rock':
+                print(x, '\nYou lose!')
+            else:
+                print(x, '\nStandoff')
